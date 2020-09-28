61f94226028119ea3a980815dcc0a896303f3a4c
JoseTovar510 <42582683+JoseTovar510@users.noreply.github.com>
2020-09-28 15:58:45 -0700

    array randomizer

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let cars = [{
+  name: "Mustang",
+  manufacturer: "Ford"},
+{
+  name: "Corvette",
+  manufacturer: "Chevrolet"},
+{
+  name: "M4",
+  manufacturer: "BMW"},
+{
+  name: "R8",
+  manufacturer: "Audi"},
+{
+  name: "Aventador",
+  manufacturer: "Lamborghini"}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed(){
+  background(random(200, 255));
+  randomIndex = int(random(cars.length));
+  text(cars[randomIndex].name, 50, 50);
+  cars.splice(randomIndex, 1);
 }
