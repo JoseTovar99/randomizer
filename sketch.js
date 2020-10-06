ff45cbb453f83923bfd08b99b7d3065ce74b1cdd
JoseTovar510 <42582683+JoseTovar510@users.noreply.github.com>
2020-10-05 20:40:04 -0700

    button and pictures

--- sketch.js
+++ sketch.js
@@ -16,18 +16,42 @@ let cars = [{
 
 let randomIndex;
 let animating = false;
+let car = [];
+let imageCounter = 0;
+let button;
+
+function preload(){
+
+for (let i = 0; i <= 4; i++ ){
+  car[i] = loadImage(`assets/car_${i}.jpg`)
+}
+
+}
 
 function setup() {
-  createCanvas(700, 700);
-  background(220);
-  textSize(32);
+  createCanvas(600, 600);
+  background(173, 216, 230);
+  textSize(36);
+  imageMode(CENTER);
+  frameRate(5);
   text("Click To Randomize!", 50, 50);
+
+  button = createButton("Click to Randomize!");
+  button.mousePressed(buttonPressed);
 }
 
 function draw() {
 
 if (animating == true){
-  ellipse(random(width), random(height), random(50, 200));
+  clear();
+  image(car[imageCounter], width/2, height/2);
+
+  if (imageCounter < car.length - 1){
+    imageCounter++;
+    console.log(imageCounter);
+  } else {
+    imageCounter = 0;
+  }
 
 }
 
@@ -36,9 +60,10 @@ if (animating == true){
 function randomizer(){
   animating = false;
   if (cars[0]){
-  background(random(200, 255));
+  //background(random(200, 255));
   randomIndex = int(random(cars.length));
-  text(`${cars[randomIndex].name} 's manufacturer is ${cars[randomIndex].manufacturer}`, 50, 50);
+  image(random(car), width/2, height/2);
+  text(`${cars[randomIndex].name}`, width/2, height - 530);
   cars.splice(randomIndex, 1);
 } else{
   background(random(200, 255));
@@ -46,7 +71,7 @@ function randomizer(){
 }
 }
 
-function mousePressed(){
+function buttonPressed(){
   animating = true;
-  setTimeout(randomizer, 1000);
+  setTimeout(randomizer, 2000);
 }
